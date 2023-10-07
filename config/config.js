75a218878af43592a7adbb0a871ef647c372d007
jianglinyang8 <894443144@qq.com>
2023-10-07 16:01:51 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -83,9 +83,9 @@ var config = {
     portfolio: [
         // ["./images/pro-1.png", "http://1.15.234.109:8000/", "个人博客", "这里记录了我的Java后端学习笔记<br>持续更新"],
         // ["./images/pro-2.png", "https://github.com/happysnaker/Gobang", "智能人机对战五子棋", "采用C++编写的智能五子棋人机对战<br>2021/7/23"],
-        ["./images/ran-1.png", "https://jianglinyang8.github.io/Resume", "主管", "成品"],
-        ["./images/ran-2.png", "https://jianglinyang8.github.io/Resume", "对样", "无色差"],
-        ["./images/ran-3.png", "https://jianglinyang8.github.io/Resume", "机缸", "领班"]
+        ["./images/ran-1.jpeg", "https://jianglinyang8.github.io/Resume", "主管", "成品"],
+        ["./images/ran-2.jpeg", "https://jianglinyang8.github.io/Resume", "对样", "无色差"],
+        ["./images/ran-3.jpeg", "https://jianglinyang8.github.io/Resume", "机缸", "领班"]
     ],
 
 
